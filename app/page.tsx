4fdfbce9f4339a87473b7b96eecb57ac3b11b78c
ochosteve <ochosteve08@gmail.com>
2023-08-09 09:33:52 +0100

    create about page route and home page redirect

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
-import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      HELLO WORLD
+    <main className="flex  flex-col items-center justify-between p-24">
+      <h1>HELLO WORLD</h1>
+      <Link href={"/about"}>Go to About Page</Link>
     </main>
   );
 }
