8a88913fc0eb145bc3a50d5001904833a5e9f015
ochosteve <ochosteve08@gmail.com>
2023-08-10 21:41:45 +0100

    create the search result interface

--- app/[searchTerm]/page.tsx
+++ app/[searchTerm]/page.tsx
@@ -1,5 +1,6 @@
 import getWikiResult from "@/lib/getWikiResult";
 import type { Metadata } from "next";
+import Item from "./components/Item";
 
 type Props = {
   params: {
@@ -30,18 +31,17 @@ const SearchResult = async ({ params: { searchTerm } }: Props) => {
   const results: Result[] | undefined = data?.query?.pages;
 
   const content = (
-    <main className="bg-slate-400 mx-auto max-w-lg py-1 min-h-screen">
+    <main className="bg-slate-400 mx-auto max-w-lg py-1 min-h-screen text-black">
       {results ? (
         Object.values(results).map((result) => {
-          return <p key={result.pageid}>{result.title}</p>;
+          return <Item key={result.pageid} result={result} />;
         })
       ) : (
-        <p>{`${searchTerm} Not Found`}</p>
+        <p className="mt-4 text-center font-semibold">{`${searchTerm} Not Found`}</p>
       )}
     </main>
   );
-  console.log(results);
-  console.log(content);
+
 
   return content;
 };
