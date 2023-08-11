c19c44390c5c70bda3ee8adb2b19419fa5800a26
ochosteve <ochosteve08@gmail.com>
2023-08-11 09:08:22 +0100

    create profile component and add personal potrait

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,16 +1,19 @@
-import Link from "next/link"
-import Search from "./Search"
-
+import Link from "next/link";
 
 const Navbar = () => {
   return (
-    <nav className="bg-slate-600 p-4 flex justify-between flex-col md:flex-row sticky top-0 drop-shadow-xl">
-        <h1 className="text-3xl font-bold text-white grid place-content-center mb-2 md:mb-0">
-            <Link href={'/'}>WikiRocket!</Link>
-        </h1>
-        <Search/>
+    <nav className="bg-slate-600 p-4 sticky top-0 drop-shadow-xl z-10">
+      <div className=" mx-auto flex max-w-prose justify-between flex-col sm:flex-row">
+        <Link
+          href={"/"}
+          className="text-white/90 no-underline hover:text-white"
+        >
+          {" "}
+          Stephen Ujah
+        </Link>
+      </div>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
